3762e3c39f5dfaf21c3f7b36992f6980d21562d1
IP-C <138649184+IP-C@users.noreply.github.com>
2023-09-22 15:23:20 +0900

    エラーハンドリング関係

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,28 +1,25 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # Deviseのモジュールを設定
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   # ニックネームのバリデーションを追加
-  #validates :nickname, presence: true
+  validates :nickname, presence: true
 
   # メールアドレスのバリデーション
-  #validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
+  validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }, uniqueness: true
 
   # パスワードのバリデーション
-  #validates :password, presence: true, length: { minimum: 6 }
-  #validates :password_confirmation, presence: true
+  validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)/ }
 
   # お名前カナのバリデーション
-  #validates :last_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/ }
-  #validates :first_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/ }
+  validates :last_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/ }
 
   # お名前（漢字）のバリデーション
- # validates :last_name_kanji, presence: true
-  #validates :first_name_kanji, presence: true
+  validates :last_name_kanji, presence: true
+  validates :first_name_kanji, presence: true
 
   # 生年月日のバリデーション
-  #validates :birth_date, presence: true
+  validates :birth_date, presence: true
 end
-
