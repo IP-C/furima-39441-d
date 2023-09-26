e5ebc67c7f231aa29cb2f033930cd95315419968
IP-C <138649184+IP-C@users.noreply.github.com>
2023-09-26 17:09:02 +0900

    再編集03

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,12 +3,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before do
+    # ファクトリーを使用してテストデータを生成し、@user変数に格納
+    @user = FactoryBot.build(:user)
+  end
+
   describe 'validations' do
     it 'is valid with valid attributes' do
-      # ファクトリーを使用してテストデータを生成
-      @user = FactoryBot.build(:user)
-
-      # ユーザーオブジェクトが有効であることを検証
+      # すでにbeforeブロックで生成された@userを使用
       expect(@user).to be_valid
     end
 
@@ -23,28 +25,32 @@ RSpec.describe User, type: :model do
 
       it 'nicknameが空では登録できない' do
         @user.nickname = '' # nicknameを空に設定
-        expect(@user).not_to be_valid # ユーザーが有効でないことを検証
+        @user.valid? # ユーザーが無効であることを検証
+        expect(@user.errors[:nickname]).to include("can't be blank") # エラーメッセージを確認
       end
 
       it 'emailが空では登録できない' do
         @user.email = ''
-        expect(@user).not_to be_valid
+        @user.valid?
+        expect(@user.errors[:email]).to include("can't be blank")
       end
 
       it 'passwordが空では登録できない' do
         @user.password = ''
-        expect(@user).not_to be_valid
+        @user.valid?
+        expect(@user.errors[:password]).to include("can't be blank")
       end
 
       it 'passwordとpassword_confirmationが不一致では登録できない' do
         @user.password_confirmation = 'mismatched_password'
-        expect(@user).not_to be_valid
+        @user.valid?
+        expect(@user.errors[:password_confirmation]).to include("doesn't match Password")
       end
 
       it 'nicknameが7文字以上では登録できない' do
-        @user.nickname = 'a' * 8
-        expect(@user).not_to be_valid
-      end
+        @user.nickname = 'a' * 8 # nicknameを7文字以上に設定
+        @user.valid?
+      end      
 
       it '重複したemailが存在する場合は登録できない' do
         # ダミーユーザーを作成
@@ -52,25 +58,55 @@ RSpec.describe User, type: :model do
 
         # 重複したemailでユーザーを生成し、無効であることを検証
         @user.email = 'test@example.com'
-        expect(@user).not_to be_valid
+        @user.valid?
+        expect(@user.errors[:email]).to include('has already been taken')
       end
 
       it 'emailは@を含まないと登録できない' do
         @user.email = 'email_without_at.com'
-        expect(@user).not_to be_valid
+        @user.valid?
+        expect(@user.errors[:email]).to include('is invalid')
       end
 
       it 'passwordが5文字以下では登録できない' do
         @user.password = '12345'
-        expect(@user).not_to be_valid
+        @user.valid?
+        expect(@user.errors[:password]).to include('is too short (minimum is 6 characters)')
       end
 
       it 'passwordが129文字以上では登録できない' do
         long_password = 'a' * 130
         @user.password = long_password
-        expect(@user).not_to be_valid
+        @user.valid?
+        expect(@user.errors[:password]).to include('is too long (maximum is 128 characters)')
       end
+
+      # 以下、新たに追加した異常系テストケース
+      it '英字のみのパスワードでは登録できない' do
+        @user.password = 'abcdef'
+        @user.valid?
+        expect(@user.errors[:password]).to include('is invalid')
+      end
+
+      it '数字のみのパスワードでは登録できない' do
+        @user.password = '123456'
+        @user.valid?
+        expect(@user.errors[:password]).to include('is invalid')
+      end
+
+      it '全角文字を含むパスワードでは登録できない' do
+        @user.password = 'パスワード123'
+        @user.valid?
+        expect(@user.errors[:password]).to include('is invalid')
+      end
+
+      it '姓（全角）が空だと登録できない' do
+        @user.last_name_kanji = ''
+        @user.valid?
+        expect(@user.errors[:last_name_kanji]).to include("can't be blank")
+      end
+
+      # 他の異常系テストケースも同様に追加
     end
   end
 end
-
