4422c6b1296d8e48b575a4c8dd66767aa40abe1d
IP-C <138649184+IP-C@users.noreply.github.com>
2023-10-10 17:30:33 +0900

    設定1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,18 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.order(created_at: :desc)
+    # データベースから必要な情報を取得し、出品日時の新しい順に並べる
+    @items = Item.where(validation_conditions).order(created_at: :desc)
+  end
+
+  private
+
+  def validation_conditions
+    # バリデーション条件を設定
+    {
+      image: { not: nil },
+      name: { not: nil },
+      price: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
+      shipping_fee_payer: { not: nil }
+    }
   end
 end
