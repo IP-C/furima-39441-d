25b57601ba0a4d2e367c5a8fe9410cdec1bd18d7
IP-C <138649184+IP-C@users.noreply.github.com>
2023-10-11 13:27:00 +0900

    表示機能と売り切れ表示設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+
   def new
     @item = Item.new
   end
@@ -7,12 +8,15 @@ class ItemsController < ApplicationController
   def create
     @item = current_user.items.new(item_params)
     if @item.save
-      # 商品の保存に成功した場合の処理
       redirect_to root_path, notice: '商品が出品されました。'
     else
-      # 商品の保存に失敗した場合の処理
       render 'new'
     end
+  end
+
+  def index
+    # 全ての商品を取得
+    @items = Item.order(created_at: :desc)
   end
 
   private
@@ -20,10 +24,4 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :item_info, :category_id, :condition_id, :shipping_id, :prefecture_id, :delivery_day_id, :price, :image)
   end
-
-  def index
-    # ログイン状態にかかわらず、全ての商品を取得する
-    @items = Item.order(created_at: :desc)
-  end
 end
-
